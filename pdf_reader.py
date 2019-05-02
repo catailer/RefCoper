8f53017ae22b372cf75eb91748b826ebb86b5c44
Draftbk <draftbks@gmail.com>
2019-05-02 14:22:46 -0400

    get the reference list

--- pdf_reader.py
+++ pdf_reader.py
@@ -4,6 +4,8 @@ from pdfminer.converter import TextConverter
 from pdfminer.pdfinterp import PDFPageInterpreter
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfpage import PDFPage
+from pdfminer.layout import LAParams
+import re
 
 def extract_text_by_page(pdf_path):
     with open(pdf_path, 'rb') as fh:
@@ -12,7 +14,8 @@ def extract_text_by_page(pdf_path):
                                       check_extractable=True):
             resource_manager = PDFResourceManager()
             fake_file_handle = io.StringIO()
-            converter = TextConverter(resource_manager, fake_file_handle)
+            laparams = LAParams()
+            converter = TextConverter(resource_manager, fake_file_handle,laparams=laparams)
             page_interpreter = PDFPageInterpreter(resource_manager, converter)
             page_interpreter.process_page(page)
 
@@ -24,11 +27,29 @@ def extract_text_by_page(pdf_path):
             fake_file_handle.close()
 
 def extract_text(pdf_path):
-    my_list=[]
+    result=""
     for page in extract_text_by_page(pdf_path):
-        my_list.append(page)
-    print("1")
+        result=result+page
+    return result
+
+def get_references(article):
+    references=[]
+    start=False
+    tmp=""
+    for line in article.splitlines( ):
+        if start:
+            if re.match("(\\[).*?(\\])", line, flags=0):
+                references.append(tmp)
+                tmp=line
+            else:
+                tmp=tmp+line
+        if (line.strip().lower()=="references"):
+            start=True
+    return references
+
 
 
 if __name__ == '__main__':
-    print(extract_text('test/CCIS.pdf'))
+    article=extract_text('test/CCIS.pdf')
+    references=get_references(article)
+    print(references)
